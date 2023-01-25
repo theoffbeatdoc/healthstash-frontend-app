feff94a7f89e0a7b7372380b2165d3d0d705caed
The OffBeat Doc™ <drkoustavsinharay@gmail.com>
2023-01-25 16:39:56 +0530

    changed site config to my notion

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '23932f24c7ea4071af83cd62f587b305',
+  rootNotionPageId: 'a9cd5fde09d1448fb8419f1514470361',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
