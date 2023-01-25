3b3490e234ebc1d16d3b338bae67ab330ec4d8ba
The OffBeat Doc™ <drkoustavsinharay@gmail.com>
2023-01-25 16:58:52 +0530

    🤔

--- site.config.ts
+++ site.config.ts
@@ -3,7 +3,7 @@ import { siteConfig } from './lib/site-config'
 export default siteConfig({
   // the site's root Notion page (required)
   rootNotionPageId: 'a9cd5fde09d1448fb8419f1514470361',
-
+  //MYID a9cd5fde09d1448fb8419f1514470361
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
